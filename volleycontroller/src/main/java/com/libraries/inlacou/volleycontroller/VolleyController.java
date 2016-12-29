f9de2d277b62cacba000e456a017fb2328c998b4
Inlacou <inlacou@gmail.com>
2016-12-29 15:40:25 +0100

    Bugfix

--- volleycontroller/src/main/java/com/libraries/inlacou/volleycontroller/VolleyController.java
+++ volleycontroller/src/main/java/com/libraries/inlacou/volleycontroller/VolleyController.java
@@ -138,7 +138,7 @@ public class VolleyController {
 		Log.d(DEBUG_TAG + ".onCall." + iCall.getMethod(), "Request para la " + (primaryRequestQueue ? "primera" : "segunda") + " requestQueue creada con codigo: " + iCall.getCode());
 		Log.d(DEBUG_TAG + ".onCall."+iCall.getMethod()+"", "Making "+iCall.getMethod()+" call to url: " + iCall.getUrl());
 		logMap(iCall.getHeaders(), "header", iCall.getMethod().toString());
-		logMap(iCall.getHeaders(), "params", iCall.getMethod().toString());
+		logMap(iCall.getParams(), "params", iCall.getMethod().toString());
 		Log.d(DEBUG_TAG+".onCall."+iCall.getMethod()+"", "Rawbody: "+iCall.getRawBody());
 
 		RequestQueue mRequestQueue;
